8c30f45db7fc3ce377cefc3d1787f89c019b8b20
himihiromu <himihiromu@iclloud.com>
2020-11-10 12:31:11 +0900

    ちょっと更新

--- nameWakander_pickup.py
+++ nameWakander_pickup.py
@@ -1,5 +1,4 @@
 def write_pickup_file(nameWakander_file_path, pickup_file_path, pickup_word):
-    nameWakander_file_path
     discovery = False
     with open(pickup_file_path, mode="W") as wf:
         with open(nameWakander_file_path) as f:
@@ -11,3 +10,38 @@ def write_pickup_file(nameWakander_file_path, pickup_file_path, pickup_word):
             if discovery:
                 wf.write(s.split(",")[0])
 
+def write_cutall_tool_pickup_file(nameWakander_file_path, pickup_file_path):
+    discovery = False
+    with open(pickup_file_path, mode="W") as wf:
+        with open(nameWakander_file_path) as f:
+            s = f.readline()
+            if s in "axe" and not(s in "pick"):
+                discovery = True
+            elif s[0] != " ":
+                discovery = False
+            if discovery:
+                wf.write(s.split(",")[0])
+
+def write_mineall_tool_pickup_file(nameWakander_file_path, pickup_file_path):
+    discovery = False
+    with open(pickup_file_path, mode="W") as wf:
+        with open(nameWakander_file_path) as f:
+            s = f.readline()
+            if s in "pickaxe":
+                discovery = True
+            elif s[0] != " ":
+                discovery = False
+            if discovery:
+                wf.write(s.split(",")[0])
+
+def write_mineall_block_pickup_file(nameWakander_file_path, pickup_file_path):
+    discovery = False
+    with open(pickup_file_path, mode="W") as wf:
+        with open(nameWakander_file_path) as f:
+            s = f.readline()
+            if s in "ore":
+                discovery = True
+            elif s[0] != " ":
+                discovery = False
+            if discovery:
+                wf.write(s.split(",")[0])
